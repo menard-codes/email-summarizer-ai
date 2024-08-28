73cbbeb32f251f9c0732c14b1b2f6186bf3f3dc0
Menard Maranan <menardmaranan.codes@gmail.com>
2024-08-29 00:31:30 +0800

    inbox

--- app/routes/home.thread.$id/route.tsx
+++ app/routes/home.thread.$id/route.tsx
@@ -1,39 +1,39 @@
-import { LinksFunction } from "@remix-run/node";
+import { LoaderFunctionArgs } from "@remix-run/node";
 import styles from "./thread.styles.css?url";
-import { useOutletContext } from "@remix-run/react";
+import { requireAuth } from "~/services/auth.server";
+import { nylas } from "~/services/nylas.server";
+import { Form, useLoaderData, } from "@remix-run/react";
 
-export const links: LinksFunction = () => [
-    { rel: 'stylesheet', href: styles }
-]
+// export const links: LinksFunction = () => [
+//     { rel: 'stylesheet', href: styles }
+// ]
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+    const { id } = params;
+
+    if (!id) {
+        throw new Response('Not Found', { status: 404 });
+    }
+
+    const { grantId } = await requireAuth(request) as { grantId: string; email: string; };
+
+    const messages = await nylas.messages.list({
+        identifier: grantId,
+        queryParams: {
+            limit: 10,
+            threadId: id
+        }
+    });
+    
+    return {messages};
+}
 
 export default function Thread() {
-    const context = useOutletContext();
+    const { messages } = useLoaderData<typeof loader>();
 
     const emailThreadSummary = `
         Culpa dolor sunt consectetur ut. Adipisicing proident commodo Lorem reprehenderit sint. Ea aliqua qui cupidatat enim sint officia velit adipisicing tempor elit labore do sunt. Quis excepteur laborum velit quis aliquip in ex mollit amet esse quis deserunt. Anim non pariatur voluptate aliquip ad culpa.
     `;
-    const emails = [
-        {
-            id: 1000,
-            email: 'Cupidatat laboris esse id commodo commodo est est id aliquip.'
-        },
-        {
-            id: 1001,
-            email: 'Cupidatat laboris esse id commodo commodo est est id aliquip.'
-        },
-        {
-            id: 1002,
-            email: 'Cupidatat laboris esse id commodo commodo est est id aliquip.'
-        },
-        {
-            id: 1003,
-            email: 'Cupidatat laboris esse id commodo commodo est est id aliquip.'
-        },
-        {
-            id: 1004,
-            email: 'Cupidatat laboris esse id commodo commodo est est id aliquip.'
-        },
-    ];
     const conversation = [
         {
             id: 100,
@@ -48,45 +48,55 @@ export default function Thread() {
     ];
 
     return (
-        <div className="threads-body">
-            <div className="threads-section">
-                <h2>Email Thread Summary</h2>
-                <p>{emailThreadSummary}</p>
-                <hr />
-                <h2>Email Thread</h2>
-                <div className="threads">
-                    {
-                        emails.map(({email, id}) => (
-                            <p key={id} className="email-thread-item">{email}</p>
-                        ))
-                    }
+        <div className="grid grid-cols-2 max-w-6xl mx-auto py-4">
+            <div className="border-r-2 p-4 grid grid-rows-[auto_auto_1fr]">
+                <div>
+                    <h2 className="font-semibold text-xl mb-2">Email Thread Summary</h2>
+                    <p className="indent-4">{emailThreadSummary}</p>
+                </div>
+                <hr className="my-4" />
+                <div className="grid grid-rows-[auto_1fr]">
+                    <h2 className="font-semibold text-xl mb-2">Email Thread</h2>
+                    <div className="relative">
+                        <div className="absolute top-0 left-0 w-full h-full overflow-y-auto">
+                            {
+                                messages.data.map(msg => (
+                                    msg.body as string
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="chat-section">
-                <div>
-                    <h2>Chat</h2>
+            <div className="p-4 pl-0 grid grid-rows-[auto_1fr_auto]">
+                <div className="shadow-md pl-4">
+                    <h2 className="font-semibold text-xl mb-2">Chat</h2>
                     <p>Ask anything about this thread.</p>
                 </div>
-                <div className="conversation">
-                    {
-                        conversation.map(({ id, message, role }) => (
-                            <p
-                                key={id}
-                                className={'chat-bubble ' + (role === 'user' ? 'u-chat-bubble'  : role === 'system' ? 's-chat-bubble'  : '')}
-                            >
-                                {message}
-                            </p>
-                        ))
-                    }
+                <div className="relative">
+                    <div className="absolute w-full h-full overflow-y-auto p-1 pl-4">
+                        {
+                            conversation.map(({ id, message, role }) => (
+                                <p
+                                    key={id}
+                                    className={`w-fit max-w-[70%] my-4 p-4 rounded-2xl ${(role === 'user' ? 'bg-gray-800 text-gray-200 ml-auto'  : role === 'system' ? 'bg-gray-400 mr-auto'  : '')}`}
+                                >
+                                    {message}
+                                </p>
+                            ))
+                        }
+                    </div>
                 </div>
-                <div className="chat-form">
+                <Form
+                    className="pl-4 grid grid-cols-[1fr_auto] gap-4 pt-2"
+                >
                     <input
                         type="text"
                         placeholder="Chat about the thread"
-                        className="chat-input"
+                        className="block w-full border rounded-lg p-2 outline-none"
                     />
-                    <button>send</button>
-                </div>
+                    <button type="submit" className="px-4 border rounded-lg">Send</button>
+                </Form>
             </div>
         </div>
     )
--- app/routes/home/route.tsx
+++ app/routes/home/route.tsx
@@ -1,88 +1,152 @@
-import { MetaFunction, LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
-import styles from "./home.styles.css?url";
-import { Form, Outlet, useLoaderData, useLocation } from "@remix-run/react";
+import { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, Link, Outlet, useLoaderData, useLocation } from "@remix-run/react";
 import { requireAuth } from "~/services/auth.server";
+import { nylas } from "~/services/nylas.server";
 
 export const meta: MetaFunction = () => [
     { title: 'Email Summarizer AI' },
     { name: 'description', content: 'Email Summarizer AI' }
 ]
 
-export const links: LinksFunction = () => [
-    { rel: 'stylesheet', href: styles }
-]
-
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-    const user = await requireAuth(request);
-    return user;
+    const { email, grantId } = await requireAuth(request) as { grantId: string; email: string; };
+
+    const threads = await nylas.threads.list({
+        identifier: grantId,
+        queryParams: {
+            limit: 10
+        }
+    });
+
+    return { email, threads: threads.data.map(({ id, unread, subject, snippet }) => ({ id, unread, subject, snippet })) };
 }
 
 export default function Home() {
-    const { email, grantId } = useLoaderData<typeof loader>();
-
-    const emailThreads = [
-        {
-            text: 'Thread 1',
-            id: 1111
-        },
-        {
-            text: 'Thread 2',
-            id: 1112
-        },
-        {
-            text: 'Thread 3',
-            id: 1113
-        },
-    ];
-
+    const { email, threads } = useLoaderData<typeof loader>();
     const { pathname } = useLocation();
 
     return (
-        <main>
-            <nav>
-                <a href="/home">
-                    <h1>Email Summarizer</h1>
-                </a>
-                <div className="flex items-center gap-x-4">
-                    <p>{email}</p>
-                    <Form action="/logout" method="post">
-                        <button type="submit" className="bg-red-600 hover:bg-red-500 text-white px-3 py-1 rounded-lg">Log Out</button>
-                    </Form>
-                </div>
-            </nav>
-
-            <div className="body">
-                <aside>
-                    <p className="sidebar-label">Email Threads</p>
-                    <ul className="threads">
-                        {
-                            emailThreads.map(thread => (
-                                <a href={`/home/thread/${thread.id}`} key={thread.id}>
-                                    <li>{thread.text}</li>
-                                </a>
-                            ))
-                        }
-                    </ul>
-                </aside>
-                <div className="content">
-                    {
-                        pathname === "/home"
-                            ? (
-                                <div className="thread-template">
-                                    <h2>Select an Email Thread</h2>
-                                    <p>
-                                        Select an email thread from the list on the left to
-                                        get the summary and overview of the thread and ask
-                                        questions about that thread to get onboarded on the
-                                        discussions in no time.
-                                    </p>
-                                </div>
-                            ) : (
-                                <Outlet context={{grantId}} />
-                            )
-                    }
-                </div>
+        <main className="h-full grid grid-rows-[auto_1fr]">
+            <div className="shadow-md">
+                <nav className="flex justify-between items-center p-4 max-w-6xl mx-auto">
+                    <Link to="/home" className="hover:underline decoration-wavy text-2xl">
+                        <h1 className="font-semibold">Email Summarizer</h1>
+                    </Link>
+                    <div className="flex items-center gap-x-4">
+                        <p>{email}</p>
+                        <Form action="/logout" method="post">
+                            <button type="submit" className="bg-red-600 hover:bg-red-500 text-white px-3 py-1 rounded-lg">Log Out</button>
+                        </Form>
+                    </div>
+                </nav>
             </div>
+
+            {
+                pathname === "/home"
+                    ? (
+                        <div className="p-4 max-w-6xl mx-auto">
+                            <div>
+                                <div>
+                                    <h2 className="font-semibold text-4xl mb-2">Inbox</h2>
+                                    <div className="grid grid-cols-[1fr_auto] gap-4">
+                                        <Form>
+                                            <input
+                                                type="search"
+                                                placeholder="Search Inbox..."
+                                                className="block w-full border rounded-lg p-2 outline-none"
+                                            />
+                                        </Form>
+                                        <div>
+                                            <button className="py-2 px-4 bg-amber-500 text-gray-50 rounded-lg">&lt;</button>
+                                            <button className="mx-4 text-xl font-semibold">10</button>
+                                            <button className="py-2 px-4 bg-amber-500 text-gray-50 rounded-lg">&gt;</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                {
+                                    threads.map(({ id, unread, subject, snippet }) => (
+                                        <Link to={`/home/thread/${id}`} key={id}>
+                                            <div className={`${unread ? 'bg-gray-400' : 'bg-gray-200'} relative flex gap-3 items-center p-4 my-4 text-nowrap overflow-hidden rounded-lg`}>
+                                                <p className={`${unread ? 'font-semibold' : ''}`}>{subject}</p>
+                                                <p className="text-sm italic text-gray-600">{snippet}</p>
+
+                                                <div className={`absolute top-0 right-0 h-full w-48 bg-gradient-to-l ${unread ? 'from-gray-400' : 'from-gray-200'}`}></div>
+                                            </div>
+                                        </Link>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ) : (
+                        <Outlet />
+                    )
+            }
         </main>
     )
+
+    // const emailThreads = [
+    //     {
+    //         text: 'Thread 1',
+    //         id: 1111
+    //     },
+    //     {
+    //         text: 'Thread 2',
+    //         id: 1112
+    //     },
+    //     {
+    //         text: 'Thread 3',
+    //         id: 1113
+    //     },
+    // ];
+
+    // const { pathname } = useLocation();
+
+    // return (
+    //     <main>
+    //         <nav>
+    //             <a href="/home">
+    //                 <h1>Email Summarizer</h1>
+    //             </a>
+    //             <div className="flex items-center gap-x-4">
+    //                 <p>{email}</p>
+    //                 <Form action="/logout" method="post">
+    //                     <button type="submit" className="bg-red-600 hover:bg-red-500 text-white px-3 py-1 rounded-lg">Log Out</button>
+    //                 </Form>
+    //             </div>
+    //         </nav>
+
+    //         <div className="body">
+    //             <aside>
+    //                 <p className="sidebar-label">Email Threads</p>
+    //                 <ul className="threads">
+    //                     {
+    //                         emailThreads.map(thread => (
+    //                             <a href={`/home/thread/${thread.id}`} key={thread.id}>
+    //                                 <li>{thread.text}</li>
+    //                             </a>
+    //                         ))
+    //                     }
+    //                 </ul>
+    //             </aside>
+    //             <div className="content">
+    //                 {
+    //                     pathname === "/home"
+    //                         ? (
+    //                             <div className="thread-template">
+    //                                 <h2>Select an Email Thread</h2>
+    //                                 <p>
+    //                                     Select an email thread from the list on the left to
+    //                                     get the summary and overview of the thread and ask
+    //                                     questions about that thread to get onboarded on the
+    //                                     discussions in no time.
+    //                                 </p>
+    //                             </div>
+    //                         ) : (
+    //                             <Outlet context={{grantId}} />
+    //                         )
+    //                 }
+    //             </div>
+    //         </div>
+    //     </main>
+    // )
 }
