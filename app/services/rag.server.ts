afea31be77810a78cdd32b993476480101a7dcd7
Menard Maranan <menardmaranan.codes@gmail.com>
2024-09-01 18:49:48 +0800

    google search

--- app/services/rag.server.ts
+++ app/services/rag.server.ts
@@ -1,3 +1,5 @@
+
+import axios from "axios";
 import { chatModel } from "./gemini.server";
 
 export async function extractKeywords(query: string) {
@@ -9,4 +11,23 @@ export async function extractKeywords(query: string) {
     const results = await chatModel.generateContent(prompt);
     const keywords = results.response.text().split(',').map(keyword => keyword.trim());
     return keywords;
+}
+
+export async function googleSearch(keywords: string[]) {
+    const searchQuery = keywords.join(' ');
+    const response = await axios.get('https://www.googleapis.com/customsearch/v1', {
+        params: {
+            key: process.env.GOOGLE_SEARCH_API_KEY,
+            cx: process.env.GOOGLE_SEARCH_ENGINE_ID,
+            q: searchQuery
+        }
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const compiledSearchResults = response.data.items.map((item: any): string => {
+        const snippet = item.snippet as string;
+        const description = item.metatags["og:description"] as string || '';
+        return description ? snippet + '. ' + description : snippet;
+    }).join(' ');
+    return compiledSearchResults;
 }
